1d639a18b207f62efaccba6d2afd2533476338cb
Fxynos <sinerechka0000@gmail.com>
2025-11-07 13:01:41 +0700

    implement concurrent video editing

--- src/main/kotlin/VideoEditor.kt
+++ src/main/kotlin/VideoEditor.kt
@@ -1,5 +1,8 @@
 package com.fxynos.multiprocessing.lab1
 
+import com.fxynos.multiprocessing.lab1.common.FrameEditor
+import com.fxynos.multiprocessing.lab1.common.RgbFilterFrameEditor
+import com.fxynos.multiprocessing.lab1.common.SingleSizeFrameBufferSupplier
 import nu.pattern.OpenCV
 import org.opencv.core.Mat
 import org.opencv.core.Size
@@ -11,7 +14,8 @@ import java.util.concurrent.Executors
 import java.util.concurrent.Future
 import kotlin.concurrent.atomics.ExperimentalAtomicApi
 
-private const val THREADS_COUNT = 4
+private const val THREADS_COUNT_READ = 4
+private const val THREADS_COUNT_EDIT = 32
 private const val VID_SRC_FILE_PATH = "C:\\Users\\Fxynos\\Downloads\\mp lab video\\Cat brain failure 120s.mp4"
 private const val VID_DEST_FILE_PATH = "output.mp4"
 
@@ -29,19 +33,19 @@ fun main() {
     val frameCount = capture.get(Videoio.CAP_PROP_FRAME_COUNT).toInt()
     log("Video loaded: ${width}x$height, ${String.format("%.2f", frameCount / fps)} seconds")
 
-    val executorService = Executors.newFixedThreadPool(THREADS_COUNT)
-    val chunkSize: Int = frameCount / THREADS_COUNT
+    val executorService = Executors.newFixedThreadPool(maxOf(THREADS_COUNT_READ, THREADS_COUNT_EDIT))
+    val readChunkSize: Int = frameCount / THREADS_COUNT_READ
     val allFrames: List<Mat> = executorService.invokeAll(
-        List(THREADS_COUNT) { threadIndex ->
+        List(THREADS_COUNT_READ) { threadIndex ->
             Callable {
-                log("Chunk $threadIndex reading...")
+                log("Chunk $threadIndex is reading...")
                 val result = readChunk(
                     videoFilePath = VID_SRC_FILE_PATH,
-                    fromFrameInclusive = threadIndex * chunkSize,
-                    toFrameExclusive = if (threadIndex == THREADS_COUNT - 1)
+                    fromFrameInclusive = threadIndex * readChunkSize,
+                    toFrameExclusive = if (threadIndex == THREADS_COUNT_READ - 1)
                             frameCount
                         else
-                            (threadIndex + 1) * chunkSize
+                            (threadIndex + 1) * readChunkSize
                 )
                 log("Chunk $threadIndex is read")
                 result
@@ -49,6 +53,24 @@ fun main() {
         }
     ).flatMap(Future<List<Mat>>::get)
     log("Frames are read")
+
+    val editChunkSize: Int = allFrames.size / THREADS_COUNT_EDIT
+    executorService.invokeAll(
+        List(THREADS_COUNT_EDIT) { threadIndex ->
+            Callable {
+                log("Chunk $threadIndex is editing...")
+                editChunk(allFrames.subList(
+                    fromIndex = threadIndex * editChunkSize,
+                    toIndex = if (threadIndex == THREADS_COUNT_EDIT - 1)
+                        frameCount
+                    else
+                        (threadIndex + 1) * editChunkSize
+                )) // change input frames
+                log("Chunk $threadIndex is edited")
+            }
+        }
+    )
+    log("Frames are edited")
 
     VideoWriter(
         VID_DEST_FILE_PATH,
@@ -82,4 +104,17 @@ private fun readChunk(
                 throw IndexOutOfBoundsException("Frame $frameIndex is out of bounds")
         }
     }
+}
+
+/**
+ * Updates content of input [frames]
+ */
+private fun editChunk(frames: List<Mat>) {
+    val editor: FrameEditor = RgbFilterFrameEditor(
+        SingleSizeFrameBufferSupplier(),
+        1.5f,
+        1f,
+        0.5f
+    )
+    frames.map { editor.edit(it) }
 }
--- src/main/kotlin/common/IndexedFrame.kt
+++
@@ -1,5 +0,0 @@
-package com.fxynos.multiprocessing.lab1.common
-
-import org.opencv.core.Mat
-
-data class IndexedFrame(val index: Int, val frame: Mat)
